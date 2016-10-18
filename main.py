0ec135fae931de114969f36c4d3c41f35f04681b
Wig C <onlywig@gmail.com>
2016-10-18 16:11:48 +0800

    st7b2: add items and status parser to front-end.

--- main.py
+++ main.py
@@ -11,7 +11,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template('web/index.html')
+
+	proc = subprocess.Popen(["cat ./tn-qc/st7b2.conf;", ""], stdout=subprocess.PIPE, shell=True)
+	(out, err) = proc.communicate()
+	test_items=out.split("\n")
+
+	proc = subprocess.Popen(["cat ./tn-qc/results", ""], stdout=subprocess.PIPE, shell=True)
+	(out, err) = proc.communicate()
+	state_items=out.split("\n")
+
+	templateData = {
+            'test_items': test_items,
+            'state_items': state_items,
+	}
+
+	return render_template('web/index.html', **templateData)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+        app.run(host='0.0.0.0', port=8080, debug=True)

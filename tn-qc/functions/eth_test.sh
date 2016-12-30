6330bee437e4282508aa59cb626a49eba1704e3e
Wig C <onlywig@gmail.com>
2016-12-30 17:34:34 +0800

    ethernet test: improve testing script.
    Author: Bill

--- tn-qc/functions/eth_test.sh
+++ tn-qc/functions/eth_test.sh
@@ -1,12 +1,29 @@
 #!/bin/sh
 
-while true; do
+# Author: Bill
 
-if [[ "$(ifconfig | grep eth0)" ]]; then
-        echo "Pass" > results/Ethernet/result.txt
-else
-        echo "Failed" > results/Ethernet/result.txt
-fi
+while [ true ]
 
-sleep 2
+do
+
+  ping 8.8.8.8 -c 20 > results/Ethernet/pingwan.log
+
+  if [[ "$(cat results/Ethernet/pingwan.log | grep "20 packets received")" ]]; then
+    echo "Pass" > results/Ethernet/result.txt
+    exit
+
+  else
+
+    if [[ "$(cat results/Ethernet/pingwan.log | grep "19 packets received")" ]]; then
+      echo "Pass" > results/Ethernet/result.txt
+      exit
+
+    fi
+
+    echo "Failed" > results/Ethernet/result.txt
+
+  fi
+
+  sleep 5
+
 done

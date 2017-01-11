18a068d54225e8f8683bc1338e87e43ed3b14275
Wig C <onlywig@gmail.com>
2017-01-11 18:36:15 +0800

    led: change the gpio.

--- tn-qc/functions/led_test.sh
+++ tn-qc/functions/led_test.sh
@@ -11,27 +11,27 @@ if [[ "$res" == "1" ]]; then
 	 sh -c 'echo 0 > /sys/class/gpio/gpio96/value'
 
 	#WLAN LED
-	 sh -c 'echo 0 > /sys/class/gpio/gpio16/value'
-	res=$( sh -c 'cat /sys/class/gpio/gpio16/value')
+	 sh -c 'echo 0 > /sys/class/gpio/gpio103/value'
+	res=$( sh -c 'cat /sys/class/gpio/gpio103/value')
 
 	if [[ "$res" == "0" ]]; then
 		sleep 0.2
-		 sh -c 'echo 1 > /sys/class/gpio/gpio16/value'
+		 sh -c 'echo 1 > /sys/class/gpio/gpio103/value'
 
 		#APP LED
-		 sh -c 'echo 0 > /sys/class/gpio/gpio17/value'
-		res=$( sh -c 'cat /sys/class/gpio/gpio17/value')
+		 sh -c 'echo 0 > /sys/class/gpio/gpio104/value'
+		res=$( sh -c 'cat /sys/class/gpio/gpio104/value')
 
 		if [[ "$res" == "0" ]]; then
 			sleep 0.2
-			 sh -c 'echo 1 > /sys/class/gpio/gpio17/value'
+			 sh -c 'echo 1 > /sys/class/gpio/gpio104/value'
 			echo "Pass" > results/LED/result.txt
 		else
-			 sh -c 'echo 1 > /sys/class/gpio/gpio17/value'
+			 sh -c 'echo 1 > /sys/class/gpio/gpio104/value'
 			echo "Failed" > results/LED/result.txt
 		fi
 	else
-		 sh -c 'echo 1 > /sys/class/gpio/gpio16/value'
+		 sh -c 'echo 1 > /sys/class/gpio/gpio103/value'
 		echo "Failed" > results/LED/result.txt
 	fi
 

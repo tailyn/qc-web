80eaa1ded81f91318eeca2d7b56348e21849ee25
Wig C <onlywig@gmail.com>
2017-02-13 14:27:36 +0800

    mac scan: add mac address scanning function.

---
+++ tn-qc/functions/getmac.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Original Author: Bill
+
+
+mac_path=/root/qc-web/tn-qc/results/MACs
+
+while [ true ]
+do
+    echo "$(ifconfig eth0 | grep HWaddr | awk '{print $5}')" > "$mac_path"/mac_eth0
+    echo "$(ifconfig eth1 | grep HWaddr | awk '{print $5}')" > "$mac_path"/mac_eth1
+    echo "$(ifconfig wlan0 | grep HWaddr | awk '{print $5}')" > "$mac_path"/mac_wlan0
+    echo "$(hciconfig | grep Address | awk '{print $3}')" > "$mac_path"/mac_BT
+done

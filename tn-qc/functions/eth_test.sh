02344d2befe369c973f495c098c52f44a3d1e457
Wig C <onlywig@gmail.com>
2017-03-07 15:50:16 +0800

    eth_test: imporve the ethernet test.
    add 100M speed test after 1G speed test.

--- tn-qc/functions/eth_test.sh
+++ tn-qc/functions/eth_test.sh
@@ -9,21 +9,22 @@ do
   ping 10.20.30.1 -c 20 > results/Ethernet/pingwan.log
 
   if [[ "$(cat results/Ethernet/pingwan.log | grep "20 packets received")" ]]; then
-    echo "Pass" > results/Ethernet/result.txt
-    exit
+    ethtool -s eth1 speed 100 duplex full
+    sleep 1
+	ping 10.20.30.1 -c 20 > results/Ethernet/pingwan.log
+        if [[ "$(cat results/Ethernet/pingwan.log | grep "20 packets received")" ]]; then
+		echo "Pass" > results/Ethernet/result.txt
+		exit
+	else
+		echo "Failed" > results/Ethernet/result.txt
+	fi
 
   else
-
-    if [[ "$(cat results/Ethernet/pingwan.log | grep "19 packets received")" ]]; then
-      echo "Pass" > results/Ethernet/result.txt
-      exit
-
-    fi
 
     echo "Failed" > results/Ethernet/result.txt
 
   fi
 
-  sleep 5
+  sleep 2
 
 done
